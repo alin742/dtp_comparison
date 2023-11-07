fbe8c26516f657d317e57b50ff233d7137d908c5
Ali Elnwegy <alinwegy@gmail.com>
2023-11-07 06:19:56 +0100

    sending and recieving data over serial

--- Serial/Client/main.c
+++ Serial/Client/main.c
@@ -1,23 +1,80 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
+#include <strings.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <termios.h>
 
+#define BAUDRATE B921600
+#define PORT "/dev/ttyUSB0"
+#define BUFFER_CAP 256
+
+char buffer[BUFFER_CAP] = {0};
+int buffer_sz = 0;
 
 int
 openPort(char *port)
 {
   int fd = open(port, O_RDWR | O_NOCTTY | O_NDELAY);
-  if (fd == -1) perror("[ERROR] openPort: Could not open port");
+  if (fd == -1) {
+    perror("[ERROR] openPort: Could not open port");
+    exit(-1);
+  }
   fcntl(fd, F_SETFL, 0);
   return fd;
+}
+
+void
+setPortOptions(struct termios *options, int fd)
+{
+  if (tcgetattr(fd, options) < 0) {
+    perror("[ERROR] setPortOptions: Could not get port attributes");
+    exit(-1);
+  }
+  cfmakeraw(options);
+  cfsetispeed(options, BAUDRATE);
+  options->c_cflag |= CLOCAL;          // always set
+  options->c_cflag |= CREAD;           // always set
+  options->c_cflag |= CRTSCTS;         // enable rts/cts
+  options->c_cc[VTIME] = 0;            // no timeout
+  options->c_cc[VMIN] = 1;             // 1 char satisfies
+  if (tcsetattr(fd,TCSANOW,options) < 0) {
+      fprintf(stderr, "[ERROR] setPortOptions: could not set attributes %s\n",strerror(errno));
+      exit(-1);
+  }
+}
+
+void
+analyzeSerial(char *c)
+{
+  switch (*c) {
+    case '\r': printf("\n"); break;
+    case '\n': 
+      printf("%s", buffer);
+      bzero(buffer, BUFFER_CAP);
+      buffer_sz = 0;
+      break;
+    default:
+      buffer[buffer_sz++] = *c;
+  }
 }
 
 int
 main(int argc, char **argv)
 {
-  printf("test\n");
+  char c; // Character to read from serial
+  struct termios options; // Serial terminal options
+  int fd = openPort(PORT);
+  setPortOptions(&options, fd);
+  tcflush(fd, TCIFLUSH);
+  while(true) {
+    if (read(fd, &c, 1) == -1)
+      break;
+    analyzeSerial(&c);
+  }
   return 0;
 }
--- Serial/Server/src/server.ino
+++ Serial/Server/src/server.ino
@@ -1,19 +1,71 @@
 #include "Arduino.h"
+#include "WiFi.h"
+#include "time.h"
 
+/*
+* WIFICredentials.h: a header file that defines all
+*   the personal WIFI settings. The file contains the
+*   following defientions:
+*   
+*   #define SSID "[YOUR WIFI SSID]"
+*   #define PASS "[YOUR WIFI PASSWORD]"
+*   #define NTP_SERVER1 "[YOUR PREFFERED NTP SERVER]"
+*   #define NTP_SERVER2 "[A BACKUP NTP SERVER USUALLY pool.ntp.org]"
+*   #define GMT_OFFSET 3600 // the difference from your timezone to GMT in seconds
+*   #define DAYLIGHT_OFFSET 3600 // daylight saving offset in seconds
+*/
+#include "../../WIFICredentials.h"
 #define BAUDRATE 921600
 
-int count = 0;
+int64_t
+getTime()
+{
+  struct timeval tv_now;
+  gettimeofday(&tv_now, NULL);
+  return (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec;
+}
+
+void
+WiFiConnectingAnimation()
+{
+  for (int i = 0; i < 50; i++)
+    Serial.print(".");
+  Serial.println("");
+  Serial.println("INFO: Connecting to WiFi");
+}
+
+void
+SyncNTPServer()
+{
+  // Synchronize Time with NTP_SERVER1 and NTP_SERVER2
+  Serial.print("INFO: Syncronizing Time with NTP Server: ");
+  Serial.print(NTP_SERVER1);
+  Serial.print(", ");
+  Serial.println(NTP_SERVER2);
+  configTime(GMT_OFFSET, DAYLIGHT_OFFSET, NTP_SERVER1, NTP_SERVER2);
+  delay(2000);
+}
 
 void
 setup()
 {
   Serial.begin(BAUDRATE);
+  WiFi.begin(SSID, PASS);
+  Serial.println("INFO: Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    WiFiConnectingAnimation();
+  }
+  SyncNTPServer();
+  // disconnect WiFi as it's no longer needed
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
 }
 
 void
 loop()
 {
-  Serial.print("Test ");
-  Serial.println(count++);
-  delay(5);
+  Serial.print("Test (");
+  Serial.print(getTime());
+  Serial.println(")");
+  // delay(5);
 }
